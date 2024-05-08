510a16f7e8ce036358f265ce04a8868eef211dfc
neatnt <anitadamayanti61@gmail.com>
2024-05-08 12:39:53 +0700

    scanner add

--- additional/scanner.php
+++ additional/scanner.php
@@ -1,9 +1,27 @@
+<!--
+
+=========================================================
+* Argon Dashboard - v1.1.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Argon Dashboard - Free Dashboard for Bootstrap 4 by Creative Tim</title>
+  <title>
+    Argon Dashboard - Free Dashboard for Bootstrap 4 by Creative Tim
+  </title>
   <!-- Favicon -->
   <link href="../assets/img/brand/favicon.png" rel="icon" type="image/png">
   <!-- Fonts -->
@@ -14,11 +32,13 @@
   <!-- CSS Files -->
   <link href="../assets/css/argon-dashboard.css?v=1.1.2" rel="stylesheet" />
 </head>
-<body>
+
+<body class="">
   <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
     <div class="container-fluid">
       <!-- Toggler -->
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main"
+        aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <!-- Brand -->
@@ -28,10 +48,12 @@
       <!-- User -->
       <ul class="nav align-items-center d-md-none">
         <li class="nav-item dropdown">
-          <a class="nav-link nav-link-icon" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a class="nav-link nav-link-icon" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
             <i class="ni ni-bell-55"></i>
           </a>
-          <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right" aria-labelledby="navbar-default_dropdown_1">
+          <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right"
+            aria-labelledby="navbar-default_dropdown_1">
             <a class="dropdown-item" href="#">Action</a>
             <a class="dropdown-item" href="#">Another action</a>
             <div class="dropdown-divider"></div>
@@ -42,7 +64,8 @@
           <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <div class="media align-items-center">
               <span class="avatar avatar-sm rounded-circle">
-                <img alt="Image placeholder" src="../assets/img/theme/team-1-800x800.jpg">
+                <img alt="Image placeholder" src="../assets/img/theme/team-1-800x800.jpg
+">
               </span>
             </div>
           </a>
@@ -54,20 +77,20 @@
               <i class="ni ni-single-02"></i>
               <span>My profile</span>
             </a>
-            <a href="../additional/profile.html" the="dropdown-item">
+            <a href="../additional/profile.html" class="dropdown-item">
               <i class="ni ni-settings-gear-65"></i>
               <span>Settings</span>
             </a>
-            <a href="../additional/profile.html" the="dropdown-item">
+            <a href="../additional/profile.html" class="dropdown-item">
               <i class="ni ni-calendar-grid-58"></i>
               <span>Activity</span>
             </a>
-            <a href="../additional/profile.html" the="dropdown-item">
+            <a href="../additional/profile.html" class="dropdown-item">
               <i class="ni ni-support-16"></i>
               <span>Support</span>
             </a>
             <div class="dropdown-divider"></div>
-            <a href="#!" the="dropdown-item">
+            <a href="#!" class="dropdown-item">
               <i class="ni ni-user-run"></i>
               <span>Logout</span>
             </a>
@@ -85,7 +108,8 @@
               </a>
             </div>
             <div class="col-6 collapse-close">
-              <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+              <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#sidenav-collapse-main"
+                aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
                 <span></span>
                 <span></span>
               </button>
@@ -95,7 +119,8 @@
         <!-- Form -->
         <form class="mt-4 mb-3 d-md-none">
           <div class="input-group input-group-rounded input-group-merge">
-            <input type="search" class="form-control form-control-rounded form-control-prepended" placeholder="Search" aria-label="Search">
+            <input type="search" class="form-control form-control-rounded form-control-prepended" placeholder="Search"
+              aria-label="Search">
             <div class="input-group-prepend">
               <div class="input-group-text">
                 <span class="fa fa-search"></span>
@@ -110,18 +135,18 @@
               <i class="ni ni-tv-2 text-primary"></i> Dashboard
             </a>
           </li>
-          <li class="nav-item active">
-            <a class="nav-link active" href="scanner.php">
+          <li class="nav-item">
+            <a class="nav-link " href="scanner.php">
               <i class="ni ni-camera-compact text-pink"></i> Scanner
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link " href="prediction.php">
+          <li class="nav-item active">
+            <a class="nav-link active" href="prediction.php">
               <i class="ni ni-sound-wave text-orange"></i> Prediction
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link " href="history.php">
+            <a class="nav-link" href="history.php">
               <i class="ni ni-book-bookmark text-yellow"></i> History
             </a>
           </li>
@@ -131,7 +156,7 @@
         <!-- Heading -->
         <h6 class="navbar-heading text-muted">Documentation</h6>
         <!-- Navigation -->
-        <ul class="navbar-nav mb-md-3">          
+        <ul class="navbar-nav mb-md-3">
           <li class="nav-item">
             <a class="nav-link" href="../additional/login.html">
               <i class="ni ni-key-25 text-info"></i> Login
@@ -151,11 +176,23 @@
     <nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
       <div class="container-fluid">
         <!-- Brand -->
-        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="scanner.php">Scanner</a>
+        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="prediction.php">Prediction</a>
+        <!-- Form -->
+        <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
+          <div class="form-group mb-0">
+            <div class="input-group input-group-alternative">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fas fa-search"></i></span>
+              </div>
+              <input class="form-control" placeholder="Search" type="text">
+            </div>
+          </div>
+        </form>
         <!-- User -->
         <ul class="navbar-nav align-items-center d-none d-md-flex">
           <li class="nav-item dropdown">
-            <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+              aria-expanded="false">
               <div class="media align-items-center">
                 <span class="avatar avatar-sm rounded-circle">
                   <img alt="Image placeholder" src="../assets/img/theme/team-4-800x800.jpg">
@@ -198,27 +235,91 @@
     <!-- End Navbar -->
     <!-- Header -->
     <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+      <div class="container-fluid">
+        <div class="header-body">
+          <!-- Card stats -->
+          <div class="row">
+
+          </div>
+        </div>
+      </div>
     </div>
     <div class="container-fluid mt--7">
+      <!-- Table -->
       <div class="row">
         <div class="col">
-          <div class="card shadow border-0">
-            <div class="card-body d-flex flex-column justify-content-center align-items-center" style="height: 487px;">
-              <h3>Image Scanner</h3>
-              <input type="file" id="image-upload" accept="image/*" class="mb-2">
-              <button class="btn btn-primary" onclick="scanImage()">Scan Image</button>
-              <div id="image-preview" class="w-100 mt-3"></div>
+          <div class="card shadow">
+            <div class="card-header border-0">
+              <h3 class="mb-0">Card tables</h3>
+            </div>
+            <div class="card-body">
+              <form enctype="multipart/form-data">
+                <div class="form-group">
+                  <label for="imageUpload">Upload Image</label>
+                  <input type="file" class="form-control-file" id="imageUpload" accept="image/*">
+                </div>
+                <div class="form-group" id="imagePreviewContainer" style="display: none;">
+                  <label>Preview:</label>
+                  <img id="imagePreview" class="img-fluid">
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    <script>
+      document.getElementById('imageUpload').addEventListener('change', function () {
+        var file = this.files[0];
+        if (file) {
+          var reader = new FileReader();
+          reader.onload = function (event) {
+            var imagePreview = document.getElementById('imagePreview');
+            imagePreview.src = event.target.result;
+            document.getElementById('imagePreviewContainer').style.display = 'block';
+          };
+          reader.readAsDataURL(file);
+        }
+      });
+    </script>
+
+    <!-- Footer -->
+    <!-- Footer -->
+    <footer class="footer">
+      <div class="row align-items-center justify-content-xl-between">
+        <div class="col-xl-6">
+          <div class="copyright text-center text-xl-left text-muted">
+            &copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative
+              Tim</a>
+          </div>
+        </div>
+        <div class="col-xl-6">
+          <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link"
+                target="_blank">MIT License</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </div>
   </div>
   <!--   Core   -->
   <script src="../assets/js/plugins/jquery/dist/jquery.min.js"></script>
   <script src="../assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <!--   Optional JS   -->
-  <script src="../assets/js/plugins/clipboard/dist/clipboard.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <!--   Argon JS   -->
   <script src="../assets/js/argon-dashboard.min.js?v=1.1.2"></script>
   <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
@@ -228,26 +329,7 @@
         token: "ee6fab19c5a04ac1a32a645abde4613a",
         application: "argon-dashboard-free"
       });
-
-    function scanImage() {
-      const fileInput = document.getElementById('image-upload');
-      const imagePreview = document.getElementById('image-preview');
-
-      if (fileInput.files.length > 0) {
-        const reader = new FileReader();
-
-        reader.onload = function(e) {
-          const imgElement = document.createElement("img");
-          imgElement.src = e.target.result;
-          imgElement.style.maxWidth = "100%";
-          imgElement.style.height = "auto";
-          imagePreview.innerHTML = "";
-          imagePreview.appendChild(imgElement);
-        };
-
-        reader.readAsDataURL(fileInput.files[0]);
-      }
-    }
   </script>
 </body>
+
 </html>
